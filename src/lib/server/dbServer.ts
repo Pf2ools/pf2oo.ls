f23a9e19a0b5b17301f360f2fc780562bf8791f8
mrvauxs <mrvauxs@gmail.com>
2025-07-18 10:53:44 +0200

    fix: triplit server cannot be used with SvelteKit that easily, add indexeddb storage to local

--- src/lib/server/dbServer.ts
+++
@@ -1,7 +0,0 @@
-import { createServer, createTriplitStorageProvider } from '@triplit/server';
-
-export const startServer = await createServer({
-  storage: await createTriplitStorageProvider('lmdb'),
-  verboseLogs: true,
-  jwtSecret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ4LXRyaXBsaXQtdG9rZW4tdHlwZSI6InNlY3JldCIsIngtdHJpcGxpdC1wcm9qZWN0LWlkIjoibG9jYWwtcHJvamVjdC1pZCJ9.8Z76XXPc9esdlZb2b7NDC7IVajNXKc4eVcPsO7Ve0ug'
-});
