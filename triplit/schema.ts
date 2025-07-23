876f9c5eac003c4bfe9c4448fa22565c201f8a6b
mrvauxs <mrvauxs@gmail.com>
2025-07-23 16:41:58 +0200

    feat: fill out the rest of background schema

--- triplit/schema.ts
+++ triplit/schema.ts
@@ -1,15 +1,10 @@
 import { Schema as S } from "@triplit/client";
 
 export const schema = S.Collections({
-	users: {
-		schema: S.Schema({
-			id: S.Id(),
-			name: S.String(),
-		}),
-	},
 	source: {
 		schema: S.Schema({
 			id: S.String(),
+			type: S.String(),
 			title: S.Record({
 				full: S.String(),
 				short: S.String(),
@@ -24,18 +19,29 @@ export const schema = S.Collections({
 	background: {
 		schema: S.Schema({
 			id: S.String(),
+			type: S.String(),
 			name: S.Record({
 				primary: S.String(),
+				aliases: S.Json({ nullable: true }),
+				specifier: S.String({ nullable: true }),
 			}),
 			source: S.Record({
 				ID: S.String(),
 				page: S.Number(),
 			}),
+			reference: S.Json({ nullable: true }),
+			data: S.Json({ nullable: true }),
 		}),
 		relationships: {
 			sourceData: S.RelationOne("source", {
 				where: [["id", "=", "$source.ID"]],
 			}),
 		},
+	},
+	users: {
+		schema: S.Schema({
+			id: S.Id(),
+			name: S.String(),
+		}),
 	},
 });
