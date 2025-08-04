674cdfec6601c0c3434b17f59910467691929185
mrvauxs <mrvauxs@gmail.com>
2025-08-04 16:03:18 +0200

    feat: add database to global window

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,25 @@
-export const prerender = true;
+import type { Database } from "$lib/client/db";
+import { browser } from "$app/environment";
+import { db } from "$lib/client/db";
+
+/* === Options === */
 export const ssr = true;
+// export const prerender = true;
+// export const trailingSlash = "always";
+
+/* === Globals === */
+if (browser) {
+	window.pf2ools = {
+		database: db,
+		get db() { return this.database; },
+	};
+}
+
+declare global {
+	interface Window {
+		pf2ools: {
+			database: Database;
+			db: Database;
+		};
+	}
+}
