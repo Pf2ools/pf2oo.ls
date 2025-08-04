b6dc16221fae926a97d3bb7bbdba3e6b81cff98e
mrvauxs <mrvauxs@gmail.com>
2025-08-04 19:00:55 +0200

    feat: move load from server to client

--- src/routes/backgrounds/[doc]/+page.ts
+++ src/routes/backgrounds/[doc]/+page.ts
@@ -1,1 +1,16 @@
+import type { backgroundInfer } from "pf2ools-schema";
+import { Database, db } from "$lib/client/db";
+import { background } from "pf2ools-data/bundles/byDatatype/core/background.json";
+
+export async function entries() {
+	return background.map((x) => ({ doc: Database.idBuilder(x.name.primary, x.source.ID) }));
+}
+
+export async function load({ params }) {
+	console.log(params);
+	return {
+		doc: await db.triplit.fetchById("background", params.doc) as backgroundInfer,
+	};
+}
+
 export const prerender = "auto";
