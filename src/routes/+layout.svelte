1e457f5892ef99eb314859eb9e0140494e61776b
mrvauxs <mrvauxs@gmail.com>
2025-08-04 19:36:37 +0200

    feat: smaller version text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -105,7 +105,7 @@
 		<!-- Main -->
 		<main class="p-4 h-[calc(100dvh-3.5rem)] overflow-y-auto">
 			{@render children()}
-			<div id="version-tag" class="absolute bottom-0.5 right-1 text-[0.6rem] text-gray-500/50 font-mono select-all">
+			<div id="version-tag" class="absolute bottom-px right-0.5 text-[0.55rem] text-gray-500/50 font-mono select-all">
 				V.{version}
 			</div>
 		</main>
