ed51fcb8a46fe6c541da3a93f99ebc2ac48168db
mrvauxs <mrvauxs@gmail.com>
2025-08-05 18:34:53 +0200

    feat: add aliases handling

--- src/lib/components/background.svelte
+++ src/lib/components/background.svelte
@@ -1,20 +1,54 @@
 <script lang="ts">
+	import { Tooltip } from "@skeletonlabs/skeleton-svelte";
+
 	type Props = {
 		doc: DB["background"];
 	};
 	const { doc }: Props = $props();
+
+	let openState = $state(false);
 </script>
 
+{#snippet title()}
+	{doc.name.primary}
+	{#if doc.name.specifier}
+		({doc.name.specifier})
+	{/if}
+{/snippet}
+
 <svelte:boundary>
-	<h4 class="h4">
-		<span class="inline-block">
-			{doc.name.primary}
-		</span>
+	<header>
+		{#if !doc.name.aliases}
+			<span class="h4">
+				{@render title()}
+			</span>
+		{:else}
+			<Tooltip
+				open={openState}
+				onOpenChange={(e) => (openState = e.open)}
+				positioning={{ placement: "bottom" }}
+				triggerBase="h4"
+				contentBase="card preset-filled p-2 px-4 text-sm"
+				openDelay={200}
+				closeDelay={300}
+				closeOnClick={false}
+				closeOnPointerDown={false}
+				arrow
+				arrowClasses="preset-filled"
+			>
+				{#snippet trigger()}
+					{@render title()}<span class="text-sm align-top">*</span>
+				{/snippet}
+				{#snippet content()}
+					This {doc.type} also has the following {doc.name.aliases?.length === 1 ? "alias" : "aliases"}: {doc.name.aliases?.join(", ")}
+				{/snippet}
+			</Tooltip>
+		{/if}
 		<!-- TODO: Convert to anchor once /sources is up -->
-		<span class="anchor inline-block text-xs align-top opacity-75">
+		<span class="anchor inline-block text-xs align-top opacity-75 font-bold">
 			{doc.source.ID}
 		</span>
-	</h4>
+	</header>
 	<section class="prose max-w-full dark:prose-invert prose-p:not-first:indent-8 prose-p:mb-1 prose-p:mt-0 text-justify">
 		{#if doc.data?.entries}
 			{#each doc.data.entries as entry}
