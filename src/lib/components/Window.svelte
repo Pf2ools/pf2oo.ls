590f0d6f36265f52a72bf3c97aad19839e2657fe
mrvauxs <mrvauxs@gmail.com>
2025-08-05 22:42:24 +0200

    feat: add Window element

---
+++ src/lib/components/Window.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	import {
+		bounds,
+		BoundsFrom,
+		ControlFrom,
+		controls,
+		draggable,
+	} from "@neodrag/svelte";
+
+	type Props = {
+		children: Snippet<[]>;
+	};
+	const { children }: Props = $props();
+</script>
+
+<div
+	{@attach draggable([
+		controls({ allow: ControlFrom.selector(".drag-handle") }),
+		bounds(BoundsFrom.viewport()),
+	])}
+>
+	<div class="drag-handle w-full bg-gray-500">
+		drag me!
+	</div>
+	{@render children()}
+</div>
