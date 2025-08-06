8a76aae17b47bab3f7e73f875fbf75c5b3d06576
mrvauxs <mrvauxs@gmail.com>
2025-08-07 01:08:10 +0200

    feat: add an actual front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,35 @@
-<main class="container mx-auto text-center">
-	<h1>Welcome to pf2oo.ls</h1>
-
-	<div class="pf2e-card p-2 rounded mx-auto w-1/2">
-		fancy card
-		<br>
-		fancy card
-		<br>
-		fancy card
-		<br>
-		fancy card
-		<br>
-		fancy card
-		<br>
-		fancy card
+<main class="container mx-auto p-16 grid grid-cols-2 gap-2">
+	<div class="grid gap-2">
+		<div class="pf2e-card p-2 rounded space-y-2 text-justify">
+			<h1 class="font-sans h4 text-center">Welcome to pf2oo.ls!</h1>
+			<hr class="hr border-primary-300-700 shadow-primary-contrast-300-700">
+			<p>
+				This website is a work in progress. What it once aims to do is be an all-encompassing wiki for all things to do with the Pathfinder 2e ruleset. Much akin to <a class="anchor" target="_blank" href="https://2e.aonprd.com/">Archives of Nethys</a>. But newer, more modern. <span class="text-xs opacity-15">Maybe even a character sheet one day.</span>
+			</p>
+		</div>
+		<div class="pf2e-card p-2 rounded space-y-2 text-justify">
+			<h1 class="font-sans h4 text-center">Ok but how do I host this?</h1>
+			<hr class="hr border-primary-300-700 shadow-primary-contrast-300-700">
+			<p>
+				The website can be hosted either by means of serving static html files on the web, or as a Node server. The latter will (eventually) allow for maintaining a database of your own, so your content doesn't disappear if you happen to clear your browser storage, as well as share your content with other people in a centralized manner.
+			</p>
+		</div>
+	</div>
+	<div class="grid gap-2">
+		<div class="pf2e-card p-2 rounded space-y-2 text-justify">
+			<h1 class="font-sans h4 text-center">Support</h1>
+			<hr class="hr border-primary-300-700 shadow-primary-contrast-300-700">
+			<p>
+				If you are eager to see this project being continued, feel free to throw a <a class="anchor" target="_blank" href="https://ko-fi.com/mrvauxs">ko-fi</a> to its creator! Otherwise, word of mouth is a great substitute.
+			</p>
+		</div>
+		<div class="pf2e-card p-2 rounded space-y-2 text-justify">
+			<h1 class="font-sans h4 text-center">Contributing Code</h1>
+			<hr class="hr border-primary-300-700 shadow-primary-contrast-300-700">
+			<p>
+				If you are interested in contributing, there's two pathways. One is contributing typo fixes and new content to the <a class="anchor" target="_blank" href="https://github.com/Pf2ools/pf2ools-data">pf2ools-data</a> repository. The other, is to contribute directly to the codebase of the website you are reading this on, <a class="anchor" target="_blank" href="https://github.com/Pf2ools/pf2oo.ls">pf2ools</a>.
+			</p>
+			<p>For the latter, basic knowledge of <a class="anchor" target="_blank" href="https://svelte.dev/docs">Svelte and SvelteKit</a> is encouraged, as well as <a class="anchor" target="_blank" href="https://www.typescriptlang.org/docs/handbook/typescript-from-scratch.html">Typescript</a>.</p> The repository also uses the <a class="anchor" target="_blank" href="https://bun.com/">Bun runtime</a> for package management and script running. So make sure that's installed.
+		</div>
 	</div>
 </main>
