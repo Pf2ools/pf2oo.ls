2c5592a60fc8d0e2fa846d84acd2c7b928f74eef
mrvauxs <mrvauxs@gmail.com>
2025-08-07 00:42:05 +0200

    feat: even fancier cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="container mx-auto text-center">
+	<h1>Welcome to pf2oo.ls</h1>
 
-<div class="pf2e-card p-2 rounded">
-	fancy card
-	<br>
-	fancy card
-	<br>
-	fancy card
-	<br>
-	fancy card
-	<br>
-	fancy card
-	<br>
-	fancy card
-</div>
+	<div class="pf2e-card p-2 rounded mx-auto w-1/2">
+		fancy card
+		<br>
+		fancy card
+		<br>
+		fancy card
+		<br>
+		fancy card
+		<br>
+		fancy card
+		<br>
+		fancy card
+	</div>
+</main>
