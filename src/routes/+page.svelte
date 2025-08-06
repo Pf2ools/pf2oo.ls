8e048781211f3102fe5e5c941a9ad0b3bb5b9cc8
mrvauxs <mrvauxs@gmail.com>
2025-08-07 01:08:44 +0200

    ugh, remove a dot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <main class="container mx-auto p-16 grid grid-cols-2 gap-2">
 	<div class="grid gap-2">
 		<div class="pf2e-card p-2 rounded space-y-2 text-justify">
-			<h1 class="font-sans h4 text-center">Welcome to pf2oo.ls!</h1>
+			<h1 class="font-sans h4 text-center">Welcome to pf2ools!</h1>
 			<hr class="hr border-primary-300-700 shadow-primary-contrast-300-700">
 			<p>
 				This website is a work in progress. What it once aims to do is be an all-encompassing wiki for all things to do with the Pathfinder 2e ruleset. Much akin to <a class="anchor" target="_blank" href="https://2e.aonprd.com/">Archives of Nethys</a>. But newer, more modern. <span class="text-xs opacity-15">Maybe even a character sheet one day.</span>
