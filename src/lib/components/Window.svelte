a4e3610c0596f833f9c13a4091ba21e14049116b
mrvauxs <mrvauxs@gmail.com>
2025-08-07 15:43:12 +0200

    transition from using neodrag to anime.js

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
+	import type { DraggableParams } from "animejs";
 	import type { Snippet } from "svelte";
-
-	import {
-		bounds,
-		BoundsFrom,
-		ControlFrom,
-		controls,
-		draggable,
-	} from "@neodrag/svelte";
+	import { createDraggable, Draggable } from "animejs";
+	import { onMount } from "svelte";
 
 	type Props = {
 		children: Snippet<[]>;
+		draggableOptions: DraggableParams;
 	};
-	const { children }: Props = $props();
+	const { children, draggableOptions }: Props = $props();
+
+	let target: HTMLElement | undefined;
+	// eslint-disable-next-line unused-imports/no-unused-vars
+	let draggable: Draggable | undefined;
+
+	onMount(() => {
+		if (!target) throw new Error("Somehow, there is no target to make draggable.");
+		draggable = createDraggable(target, {
+			trigger: ".drag-handle",
+			container: "body",
+			containerFriction: 1,
+			velocityMultiplier: 0.2,
+			...draggableOptions,
+		});
+	});
 </script>
 
 <div
-	{@attach draggable([
-		controls({ allow: ControlFrom.selector(".drag-handle") }),
-		bounds(BoundsFrom.viewport()),
-	])}
-	class="resize overflow-auto absolute w-1/3 flex flex-col"
+	bind:this={ target }
+	class="resize overflow-auto w-1/3 flex flex-col"
 >
 	<div class="drag-handle w-full bg-gray-500">
 		drag me!
