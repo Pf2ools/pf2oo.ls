dcb096f9471dbbd3a64c292b0dc1cc682f74a37b
mrvauxs <mrvauxs@gmail.com>
2025-09-08 16:48:14 +0200

    Add search to table

--- src/lib/components/NavTable.svelte
+++ src/lib/components/NavTable.svelte
@@ -3,6 +3,8 @@
 	import type { Snippet } from "svelte";
 	import { resolve } from "$app/paths";
 	import { page } from "$app/state";
+	import { Search } from "@lucide/svelte";
+	import { TableHandler } from "@vincjo/datatables";
 
 	type Props = {
 		docs: DB[DocType][];
@@ -12,9 +14,21 @@
 	};
 	// eslint-disable-next-line unused-imports/no-unused-vars
 	const { docs, row, gridCols = "grid-cols-2", navigateOnMount = false }: Props = $props();
+	const table = new TableHandler(docs, { selectBy: "id" });
 
 	function moveKeys(event: KeyboardEvent) {
+		// Don't move if focused on an input or textarea
+		if (
+			["INPUT", "TEXTAREA", "SELECT"].includes((event.target as HTMLElement).tagName)
+			|| event.metaKey
+			|| event.ctrlKey
+			|| event.altKey
+		) {
+			return;
+		}
+		// Don't move if no doc is selected
 		if (!page.params.doc) return;
+		// Find the current element and move to next/previous
 		const el = document.getElementById(decodeURIComponent(page.params.doc));
 		switch (event.key) {
 			case "ArrowDown":
@@ -36,21 +50,28 @@
 		}
 	}
 
+	// TODO:
 	function popout(event: MouseEvent) {
 		if (event.button !== 2) return; // 0 = left, 1 = middle, 2 = right
 		event.preventDefault(); // stop context menu
 	}
 
-/* TODO: Make it remember your last nav, or else on a refresh you come back to 0.
-	onMount(async () => {
-		if (navigateOnMount) await goto(resolve(`/${docs[0].type}s/[doc]`, { doc: docs[0].id }));
-	}); */
+	const search = table.createSearch();
 </script>
 
 <svelte:window onkeydown={moveKeys}></svelte:window>
 
+<div class="flex border-b border-solid border-primary-300/33">
+	<button class="btn-icon"><Search size={20}></Search></button>
+	<input
+		class="input rounded-none text-sm"
+		placeholder="Search..." type="search"
+		bind:value={ search.value } oninput={() => search.set()}
+	/>
+</div>
+
 <div class="overflow-y-scroll h-full p-2">
-	{#each docs.toSorted((a, b) => a.name.primary.localeCompare(b.name.primary)) as doc (doc.id)}
+	{#each table.rows as doc (doc.id)}
 		{@const current = decodeURIComponent(page.params.doc!) === doc.id}
 		<a
 			id={doc.id}
